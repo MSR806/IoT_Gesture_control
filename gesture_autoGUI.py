16bd6d82123680c211c065185540e83b99f6f9c8
MSR806 <msr0806@users.noreply.github.com>
2020-12-30 00:40:52 +0530

    Project done

---
+++ gesture_autoGUI.py
@@ -0,0 +1,24 @@
+import serial                                      # Serial library for serial communication
+import pyautogui                                   # pyautogui library for controlling the mouse and keyboard.
+
+Arduino_Serial = serial.Serial('com8',9600)
+
+while 1:
+        serialInput = str (Arduino_Serial.readline())
+
+        if "LeftFar" in serialInput:
+                pyautogui.keyDown('alt')  # hold down the alt key
+                pyautogui.press('tab')
+                serialInput = str (Arduino_Serial.readline())
+                if "LF0" in serialInput:
+                        pyautogui.keyUp('alt')  # release the alt key
+        if "LeftNear" in serialInput:
+                pyautogui.keyDown('ctrl')  # hold down the ctlr key
+                pyautogui.press('tab')
+                serialInput = str (Arduino_Serial.readline())
+                if "LN0" in serialInput:
+                        pyautogui.keyUp('ctrl')  # release the alt key
+        if "RightFar" in serialInput:
+                pyautogui.press('volumeup')
+        if "RightNear" in serialInput:
+                pyautogui.press('volumedown')
--- gesture_control/gesture_control.ino
+++ gesture_control/gesture_control.ino
@@ -5,7 +5,6 @@ HC_SR04 right(11, 10);
 
   int distance_left;
   int distance_right;
-  int flag_l=0;
   int flag_r=0;
 
 void setup() {
@@ -26,37 +25,33 @@ void loop() {
   distance_left = left.distance();
   distance_right = right.distance();
 
-  if(distance_left == 0)
-  flag_l = 0;
   if (distance_left >2 && distance_left <26)
   {
     distance_left = left.distance();
-    if ((distance_left >0 && distance_left <11 && flag_l ==0)){
+    if ((distance_left >0 && distance_left <11)){
         Serial.println("LeftNear");
-        flag_l = 1;
-        delay(100);
+        delay(600);
+        Serial.print("LN");
+        Serial.println(left.distance());
     }
-    else if ((distance_left >11 && distance_left <26 && flag_l ==0)){
+    else if ((distance_left >11 && distance_left <26)){
       Serial.println("LeftFar");
-      flag_l = 1;
-      delay(100);
+      delay(600);
+      Serial.print("LF");
+      Serial.println(left.distance());
     }
   }
 
-  if(distance_right == 0)
-  flag_r = 0;
   if (distance_right >2 && distance_right <26)
   {
     distance_right = right.distance();
-    if ((distance_right >0 && distance_right <11 && flag_r ==0)){
+    if ((distance_right >0 && distance_right <11)){
         Serial.println("RightNear");
-        flag_r = 1;
-        delay(100);
+        delay(200);
     }
-    else if ((distance_right >11 && distance_right <26 && flag_r ==0)){
+    else if ((distance_right >11 && distance_right <26)){
       Serial.println("RightFar");
-      flag_r = 1;
-      delay(100);
+      delay(200);
     }
   }
 
