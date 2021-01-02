5475bcef8fccaf3fdb85f9b7c29c37c3d68db25a
MSR806 <msr0806@users.noreply.github.com>
2021-01-02 09:28:52 +0530

    Update gesture_autoGUI.py

--- gesture_autoGUI.py
+++ gesture_autoGUI.py
@@ -9,16 +9,20 @@ while 1:
         if "LeftFar" in serialInput:
                 pyautogui.keyDown('alt')  # hold down the alt key
                 pyautogui.press('tab')
+                print('LF')
                 serialInput = str (Arduino_Serial.readline())
                 if "LF0" in serialInput:
                         pyautogui.keyUp('alt')  # release the alt key
         if "LeftNear" in serialInput:
                 pyautogui.keyDown('ctrl')  # hold down the ctlr key
                 pyautogui.press('tab')
+                print('LN')
                 serialInput = str (Arduino_Serial.readline())
                 if "LN0" in serialInput:
                         pyautogui.keyUp('ctrl')  # release the alt key
         if "RightFar" in serialInput:
                 pyautogui.press('volumeup')
+                print('RF')
         if "RightNear" in serialInput:
                 pyautogui.press('volumedown')
+                print('RN')
